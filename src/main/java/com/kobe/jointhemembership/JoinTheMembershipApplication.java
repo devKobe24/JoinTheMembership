a186ef9a72649c6af6e37b2262537a59cd3f4e7d
Kobe <dev.skyachieve91@gmail.com>
2025-06-16 12:06:05 +0900

    feat: first commit

---
+++ src/main/java/com/kobe/jointhemembership/JoinTheMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.kobe.jointhemembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinTheMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinTheMembershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobe/jointhemembership/JoinTheMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobe.jointhemembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinTheMembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
